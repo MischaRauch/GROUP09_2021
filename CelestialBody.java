68f04217af4211e681e90b080a66fc277cfe19d8
MatthijsDSAI <72268494+MatthijsDSAI@users.noreply.github.com>
2021-03-13 12:34:45 +0100

    FunctionInterface and SolverInterface implementation
    First implementation. Unsure whether this is actually the correct way to implement these interfaces.

---
+++ CelestialBody.java
@@ -0,0 +1,130 @@
+/*
+ * @author Matthijs Vossen
+ * @version 0.99.0
+ *
+ * This class is an implementation of the Vector3dInterface, used to represent vectors.
+ */
+
+import titan.FunctionInterface;
+import titan.Vector3dInterface;
+import titan.SolverInterface;
+import java.lang.Math;
+
+public class CelestialBody implements FunctionInterface, SolverInterface
+{
+  private double m;
+  private Vector3d x;
+  private Vector3d v;
+  private Vector3dInterface[] movement;
+
+  public CelestialBody(double mass, Vector3d x0, Vector3d v0)
+  {
+    m = mass;
+    x = x0;
+    v = v0;
+    movement = solve(this, x, 0.1, 10);
+  }
+
+  public Vector3dInterface[] getMovement()
+  {
+    return movement;
+  }
+
+  public Vector3d getCoord()
+  {
+    return x;
+  }
+
+  public double getX()
+  {
+    return x.getX();
+  }
+
+  public double getY()
+  {
+    return x.getY();
+  }
+
+  public double getZ()
+  {
+    return x.getZ();
+  }
+
+  public double getVelX()
+  {
+    return v.getX();
+  }
+
+  public double getVelY()
+  {
+    return v.getY();
+  }
+
+  public double getVelZ()
+  {
+    return v.getZ();
+  }
+
+  /*
+   * Update rule for multiple steps. Time will start at zero.
+   *
+   * @param   f       the differential equation as defined in the project manual:
+   *                  y(t) describes the position of the system at time t
+   *                  f(t, y(t)) describes the derivative of y(t) with respect to time t
+   * @param   x0      the starting location
+   * @param   h       the step size in seconds
+   * @param   nSteps  the total number of time steps
+   * @return  an array of size nSteps with all intermediate locations along the path
+   *
+   */
+  public Vector3dInterface[] solve(FunctionInterface f, Vector3dInterface x0, double h, int nSteps)
+  {
+    Vector3dInterface[] locations = new Vector3d[nSteps];
+    double t = 0;
+    for(int i = 0; i < nSteps; i++)
+    {
+      locations[i] = step(f, t, x0, h);
+      x0 = locations[i];
+      t += h;
+      System.out.println("Location after step " + (i+1) + ": " + x0);
+    }
+    return locations;
+  }
+
+  /*
+   * Update rule for one step.
+   *
+   * @param   f   the differential equation as defined in the project manual:
+   *              y(t) describes the position of the system at time t
+   *              f(t, y(t)) describes the derivative of y(t) with respect to time t
+   * @param   t   the time
+   * @param   x   the location
+   * @param   h   the step size in seconds
+   * @return  the new location after taking one step
+   *
+   */
+  public Vector3dInterface step(FunctionInterface f, double t, Vector3dInterface x, double h)
+  {
+    return f.call(h, x);
+  }
+
+  /*
+   * This is the function that represents the first derivative
+   * denoted as f in your project manual. You need to implement
+   * this function according to the laws of physics.
+   *
+   * For example, consider the state of the system at time t as
+   * this linear function,
+   * y(t) = a*t + b,
+   * then we can choose any function f whose derivative is a.
+   * The simplest such f is
+   * f(t, y(t)) = a.
+   *
+   * @param   t   the time at which to evaluate the function
+   * @param   s   the location at which to evaluate the function
+   */
+  public Vector3dInterface call(double t, Vector3dInterface s)
+  {
+    return s.add(new Vector3d(v.getX()*t, v.getY()*t, v.getZ()*t));
+  }
+}
--- TestCode.java
+++ TestCode.java
@@ -5,20 +5,18 @@
  * This class is for testing code
  */
 
+import titan.Vector3dInterface;
+
 public class TestCode
 {
   public static void main(String[] args)
   {
-    Vector3d v1 = new Vector3d(2, 3, 5);
-    System.out.println(v1);
-    System.out.println(v1.norm());
+    Vector3d ballX = new Vector3d(0, 0, 0);
+    Vector3d ballV = new Vector3d(1, 2, 1);
 
-    Vector3d v2 = new Vector3d(2, 5, -4);
-    Vector3d v3 = new Vector3d(-2, -3, -5);
+    CelestialBody ball = new CelestialBody(0, ballX, ballV);
 
-    System.out.println(v2.dist(v3));
-    System.out.println(v2.add(v3));
-
+    Vector3dInterface[] movement = ball.getMovement();
 
   }
 }
