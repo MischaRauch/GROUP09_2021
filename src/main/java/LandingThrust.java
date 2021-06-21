45c0429a4ed09d94133109949a350c7c04c2c893
Bzz77 <72268548+Bzz77@users.noreply.github.com>
2021-06-21 14:21:06 +0200

    Add some comments

--- src/main/java/LandingThrust.java
+++ src/main/java/LandingThrust.java
@@ -1,7 +1,7 @@
 public class LandingThrust {
-    private final double maxThrust = 3e7;
-    private final double landerMass = 6e3;
-    private final double vE = 2e4;
+    private final double maxThrust = 3e7; //full-burn space shuttle engine
+    private final double landerMass = 6e3; //roughly mass of Apollo lander
+    private final double vE = 2e4; //Effective exhaust velocity
     private double fuelMass;
     private double velocity;
 
@@ -9,6 +9,7 @@ public class LandingThrust {
         this.fuelMass = initialFuelMass;
     }
 
+    //Calculate thruster for landing
     public void calculateThrust(double percentage){
         double thrust = maxThrust * percentage;
         double totalMass = landerMass + fuelMass;
